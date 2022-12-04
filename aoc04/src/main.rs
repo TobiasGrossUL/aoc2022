dbee6967876bf288619927e8029a01b7ce3b1dfb
Tobias Gross <tobias.gross@ul.com>
2022-12-04 06:34:27 +0100

    refactoring

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -3,60 +3,52 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashSet;
 
-fn check_fully_contains(first: &HashSet<i32>, second: &HashSet<i32>) -> bool {
-    return first.is_subset(second) || second.is_subset(first);
+fn check_fully_contains(first_elve: &HashSet<i32>, second_elve: &HashSet<i32>) -> bool {
+    return first_elve.is_subset(second_elve) || second_elve.is_subset(first_elve);
 }
 
-fn check_overlaps(first: &HashSet<i32>, second: &HashSet<i32>) -> bool {
-    return !first.is_disjoint(second);
+fn check_overlaps(first_elve: &HashSet<i32>, second_elve: &HashSet<i32>) -> bool {
+    return !first_elve.is_disjoint(second_elve);
 }
 
-fn part_one(data: &Vec<Vec<HashSet<i32>>>) -> i32 {
-    let mut counter = 0;
-    for team in data {
-        if check_fully_contains(&team[0], &team[1]) {
-            counter += 1;
-        }
-    }
-    return counter;
+fn part_one(camp_sections: &Vec<(HashSet<i32>, HashSet<i32>)>) -> usize {
+    let fully_overlapping = camp_sections.iter().filter(|team| {check_fully_contains(&team.0, &team.1)}).count();
+    return fully_overlapping;
 }
 
-fn part_two(data: &Vec<Vec<HashSet<i32>>>) -> i32 {
-    let mut counter = 0;
-    for team in data {
-        if check_overlaps(&team[0], &team[1]) {
-            counter += 1;
-        }
-    }
-    return counter;
+fn part_two(camp_sections: &Vec<(HashSet<i32>, HashSet<i32>)>) -> usize {
+    let partially_overlapping = camp_sections.iter().filter(|team| {check_overlaps(&team.0, &team.1)}).count();
+    return partially_overlapping;
 }
 
 fn main() {
-    let all_data = parse_data();
-    let sum = part_one(&all_data);
+    let camp_sections = parse_camp_sections();
+    let sum = part_one(&camp_sections);
     println!("Solution first part: {}", sum);
 
-    let sum = part_two(&all_data);
+    let sum = part_two(&camp_sections);
     println!("Solution second part: {}", sum);
 }
 
-fn parse_data() -> Vec<Vec<HashSet<i32>>> {
+fn make_elve_space(data_repr: &str) -> HashSet<i32> {
+    let elve_space: Vec<&str> = data_repr.split("-").collect();
+    let first = elve_space[0].parse::<i32>().unwrap();
+    let last = elve_space[1].parse::<i32>().unwrap();
+    let elve_space = first..=last;
+    let elve_space: HashSet<i32> = HashSet::from_iter(elve_space);
+    return elve_space;
+}
+
+fn parse_camp_sections() -> Vec<(HashSet<i32>, HashSet<i32>)> {
     let mut result = Vec::new();
     let lines = read_lines("input").unwrap();
     for line in lines {
-        if let Ok(linedata) = line {
-            let elves: Vec<&str> = linedata.split(",").collect();
+        if let Ok(team_data) = line {
+            let elves: Vec<&str> = team_data.split(",").collect();
             assert!(elves.len() == 2);
-            let mut result_elves = Vec::new();
-            for elve in elves {
-                let elve: Vec<&str> = elve.split("-").collect();
-                let first = elve[0].parse::<i32>().unwrap();
-                let last = elve[1].parse::<i32>().unwrap();
-                let elve = first..=last;
-                let elve: HashSet<i32> = HashSet::from_iter(elve);
-                result_elves.push(elve);
-            }
-            result.push(result_elves);
+            let first_space = make_elve_space(elves[0]);
+            let second_space = make_elve_space(elves[1]);
+            result.push((first_space, second_space));
         }
     }
     return result;
