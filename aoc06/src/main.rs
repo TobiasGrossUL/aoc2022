b4d4434ccf1f816e0d36871dd16dbeb98b7d1c44
Tobias Gross <tobias.gross@ul.com>
2022-12-06 06:22:36 +0100

    first solution

--- aoc06/src/main.rs
+++ aoc06/src/main.rs
@@ -1,101 +1,44 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
-fn part_one(stacks: &Vec<Stack>, commands: &Vec<Command>) -> String {
-    let mut stacks = stacks.clone();
-    for command in commands {
-        for _ in 0..command.amount {
-            let element = stacks[command.src].pop().unwrap();
-            stacks[command.dest].push(element);
-        }
-    }
-    return stacks.iter().map(|stack| stack.last().unwrap()).collect();
-}
-
-fn part_two(stacks: &Vec<Stack>, commands: &Vec<Command>) -> String {
-    let mut stacks = stacks.clone();
-    for command in commands {
-        let mut crane: Stack = Vec::new();
-        for _ in 0..command.amount {
-            let element = stacks[command.src].pop().unwrap();
-            crane.push(element);
-        }
-        crane.reverse();
-        stacks[command.dest].append(&mut crane);
-    }
-    return stacks.iter().map(|stack| stack.last().unwrap()).collect();
-}
+use std::collections::HashSet;
 
 fn main() {
-    let (stacks, commands)  = parse_input();
+    let input = parse_input().unwrap();
+    let position = find_marker(&input, 4).unwrap();
+    println!("Solution part 1: {}", position);
 
-    let sol1 = part_one(&stacks, &commands);
-    println!("Solution part 1: {}", sol1);
-
-    let sol2 = part_two(&stacks, &commands);
-    println!("Solution part 2: {}", sol2);
+    let position = find_marker(&input, 14).unwrap();
+    println!("Solution part 2: {}", position);
 }
 
-enum ParseMode {
-    Stacks,
-    Commands,
-}
-
-struct Command {
-    amount: i32,
-    src: usize,
-    dest: usize
-}
-
-type StackElement = char;
-type Stack = Vec<StackElement>;
-
-fn parse_stacks(stacks: &mut Vec<Stack>, line: &str) {
-    if line.contains('1') {
-        return;
+fn check_marker(marker: &[char], size: usize) -> bool {
+    if marker.len() != size {
+        return false;
     }
-
-    for (index, element) in line.chars().enumerate() {
-        if !element.is_alphabetic() {
-            continue;
-        }
-        let stack_index = (index - 1) / 4;
-        stacks[stack_index].insert(0, element);
-    }
+    let token_set: HashSet<&char> = HashSet::from_iter(marker);
+    return token_set.len() == size;
 }
 
-fn parse_command(commands: &mut Vec<Command>, line: &str) {
-    let mut command = Command {amount:0, src:0, dest:0};
-    for (index, token) in line.split(' ').enumerate() {
-        match index {
-            1 => command.amount = token.parse::<i32>().unwrap(),
-            3 => command.src = token.parse::<usize>().unwrap() - 1,
-            5 => command.dest = token.parse::<usize>().unwrap() - 1,
-            _ => ()
+fn find_marker(tokens: &Vec<char>, size: usize) -> Option<usize> {
+    for i in 0..tokens.len() {
+        let marker_slice = &tokens[i..i+size];
+        let is_marker = check_marker(marker_slice, size);
+        if is_marker {
+            return Some(i + size);
         }
     }
-    commands.push(command);
+    return None;
 }
 
-fn parse_input() -> (Vec<Stack>, Vec<Command>) {
-    let mut stacks = vec![Vec::new(); 9];
-    let mut commands = Vec::new();
-    let mut mode = ParseMode::Stacks;
-
+fn parse_input() -> Option<Vec<char>> {
     for line in read_lines("input").unwrap() {
         if let Ok(linedata) = line {
-            if linedata == "" {
-                mode = ParseMode::Commands;
-                continue;
-            }
-            match mode {
-                ParseMode::Stacks => parse_stacks(&mut stacks, &linedata),
-                ParseMode::Commands => parse_command(&mut commands, &linedata)
-            }
+            let tokens = linedata.chars().collect();
+            return Some(tokens);
         }
     }
-    return (stacks, commands);
+    return None;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
