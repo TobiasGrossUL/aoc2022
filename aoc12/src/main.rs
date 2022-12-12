ca8fae371926fffcc24e00c2840b830864df570a
Tobias Gross <tobias.gross@ul.com>
2022-12-12 08:49:31 +0100

    solution part 1

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,243 +1,221 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::str::SplitWhitespace;
-use std::cell::RefCell;
 
-struct Monkey {
-    items: Vec<i64>,
-    item_operation: (Operation, Option<i64>),
-    test: i64,
-    test_true_target: usize,
-    test_false_target: usize,
-    inspections: i64,
-    devisor: i64,
-    modulo_devisor: Option<i64>
+struct Map {
+    start: (usize, usize),
+    end: (usize, usize),
+    grid: Vec<Vec<u32>>,
+    max_steps: usize,
+    visited: HashMap<(usize, usize), usize>
 }
 
-impl Monkey {
-    fn new(parts: &mut Vec<Part>) -> Monkey {
-        let mut items = Vec::new();
-        let mut item_operation = (Operation::Add, None);
-        let mut test = 0;
-        let mut test_true = 0;
-        let mut test_false = 0;
-        let inspections = 0;
-        let devisor = 3;
-        let modul = None;
-        loop {
-            if parts.is_empty() {
-                break;
+impl Map {
+    fn new(start: (usize, usize), end: (usize, usize), grid: Vec<Vec<u32>>) -> Map {
+        let max_steps = grid.len() * grid[0].len();
+        let visited = HashMap::new();
+        Map {start, end, grid, max_steps, visited}
+    }
+
+    fn _possible_steps(&self, current: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut result = Vec::new();
+        let max_elevation = self.grid[current.0][current.1] + 1;
+        if current.0 > 0 {
+            result.push((current.0 -1, current.1));
+        }
+        if current.1 > 0 {
+            result.push((current.0, current.1 -1));
+        }
+
+        if current.0 < self.grid.len() - 1 {
+            result.push((current.0 +1, current.1));
+        }
+
+        if current.1 < self.grid[0].len() - 1 {
+            result.push((current.0, current.1 +1));
+        }
+
+        return result.iter().filter(|x| self.grid[x.0][x.1] <= max_elevation).copied().collect();
+    }
+
+    // returns true if target is reached and amount of steps required
+    fn _walk(&mut self, steps: usize, position: (usize, usize)) -> (bool, usize) {
+        // check if we hit target
+        if position == self.end {
+            return (true, steps);
+        }
+
+        // check if we exceed max steps
+        if steps >= self.max_steps {
+            return (false, 0);
+        }
+
+        if self.visited.contains_key(&position){
+            if self.visited.get(&position).unwrap() > &steps {
+                self.visited.insert(position, steps);
+            } else {
+                return (false, 0);
             }
-            let part = parts.remove(0);
-            match part {
-                Part::StartItem(item) => items.push(item),
-                Part::Operation(op) => item_operation = op,
-                Part::Test(cond) => test = cond,
-                Part::TestTrue(target) => test_true = target,
-                Part::TestFalse(target) => test_false = target,
-                Part::Monkey => break
-            };
-
-        }
-        Monkey {items, item_operation, test, test_true_target: test_true, test_false_target: test_false, inspections, devisor, modulo_devisor: modul}
-    }
-
-    fn receive_item(&mut self, item: i64) {
-        self.items.push(item);
-    }
-
-    fn optimize_item(&self, item: i64) -> i64 {
-        if self.modulo_devisor.is_some() {
-            return item % self.modulo_devisor.unwrap();
-        }
-        return item
-    }
-
-    fn inspect_item(&self, item: i64) -> (i64, usize) {
-        let  param = match self.item_operation.1 {
-            Some(x) => x,
-            None => item
-        };
-        let mut new_item = match self.item_operation.0 {
-            Operation::Add => (item + param) / self.devisor,
-            Operation::Mult => (item * param) / self.devisor,
-        };
-
-        new_item = self.optimize_item(new_item);
-
-        if new_item % self.test == 0 {
-            return (new_item, self.test_true_target);
         } else {
-            return (new_item, self.test_false_target);
+            self.visited.insert(position, steps);
+        }
+
+        let next_positions = self._possible_steps(position);
+
+        let mut steps_to_finish = None;
+        for next_pos in next_positions {
+            let (reached, variant_steps) = self._walk(steps + 1, next_pos);
+            if reached {
+                if steps_to_finish.is_none() {
+                    steps_to_finish = Some(variant_steps);
+                } else {
+                    if steps_to_finish.unwrap() > variant_steps {
+                        steps_to_finish = Some(variant_steps);
+                    }
+                }
+            }
+        }
+
+        if steps_to_finish.is_some() {
+            return (true, steps_to_finish.unwrap());
+        } else {
+            return (false,0);
         }
     }
 
-    fn throw_item(item: i64, reveiver: usize, monkeys: &Vec<RefCell<Monkey>>) {
-        monkeys[reveiver].borrow_mut().receive_item(item);
-    }
-
-    fn do_monkey_business(&mut self, monkeys: &Vec<RefCell<Monkey>>) {
-        while !self.items.is_empty() {
-            let item = self.items.remove(0);
-            self.inspections += 1;
-            let (item, receiver) = self.inspect_item(item);
-            Self::throw_item(item, receiver, monkeys);
+    fn _best_reverse_steps(&self, current: &(usize, usize)) -> Vec<(usize, usize)> {
+        let mut result = Vec::new();
+        let min_elevation;
+        if self.grid[current.0][current.1] == 0 {
+            min_elevation = self.grid[current.0][current.1];
+        } else {
+            min_elevation = self.grid[current.0][current.1] - 1;
         }
 
-    }
-}
+        if current.0 > 0 {
+            result.push((current.0 -1, current.1));
+        }
+        if current.1 > 0 {
+            result.push((current.0, current.1 -1));
+        }
 
-fn build_monkeys(parts: &mut Vec<Part>) -> Vec<RefCell<Monkey>> {
-    parts.remove(0);
-    let mut result = Vec::new();
-    while !parts.is_empty() {
-        result.push(RefCell::new(Monkey::new(parts)));
+        if current.0 < self.grid.len() - 1 {
+            result.push((current.0 +1, current.1));
+        }
+
+        if current.1 < self.grid[0].len() - 1 {
+            result.push((current.0, current.1 +1));
+        }
+
+        result = result.iter().filter(|x| {
+            self.grid[x.0][x.1] >= min_elevation
+        }).copied().collect();
+        return result;
     }
-    return result;
+
+    // returns true if target is reached and amount of steps required
+    fn _walk_reverse(&mut self, steps: usize, position: &(usize, usize), max_steps: usize) -> (bool, usize) {
+        let mut local_max_steps = max_steps;
+        // check if we hit target
+        if position == &self.start {
+            return (true, steps);
+        }
+
+        if position == &self.start {
+            return (true, steps);
+        }
+
+        // check if we exceed max steps
+        if steps >= max_steps {
+            return (false, 0);
+        }
+
+        if self.visited.contains_key(&position){
+            if self.visited.get(&position).unwrap() > &steps {
+                self.visited.insert(position.clone(), steps);
+            } else {
+                return (false, 0);
+            }
+        } else {
+            self.visited.insert(position.clone(), steps);
+        }
+
+        let next_positions = self._best_reverse_steps(position);
+
+        let mut steps_to_finish = None;
+        for next_pos in next_positions {
+            let (reached, variant_steps) = self._walk_reverse(steps + 1, &next_pos, local_max_steps);
+            if reached {
+                local_max_steps = variant_steps;
+                if steps_to_finish.is_none() {
+                    steps_to_finish = Some(variant_steps);
+                } else {
+                    if steps_to_finish.unwrap() > variant_steps {
+                        steps_to_finish = Some(variant_steps);
+                    }
+                }
+            }
+        }
+
+        if steps_to_finish.is_some() {
+            return (true, steps_to_finish.unwrap());
+        } else {
+            return (false,0);
+        }
+    }
+
+    fn start_walking(&mut self) -> usize {
+        //return self._walk(0, self.start).1;
+        return self._walk_reverse(0, &self.end.clone(), self.max_steps).1;
+    }
 }
 
 fn main() {
+    test();
     part_one();
     part_two();
 }
 
-fn monkey_business(monkeys: &Vec<RefCell<Monkey>>) -> i64 {
-    let mut scores = Vec::new();
-    for monkey in monkeys {
-        scores.push(monkey.borrow().inspections);
-    }
-    scores.sort();
-    scores.reverse();
-    return scores[0] * scores[1];
+fn test() -> usize {
+    let mut map = parse_input("test_input");
+    println!("Max Steps: {}", map.max_steps);
+    let result = map.start_walking();
+    println!("Solution test: {}", result);
+    return result;
 }
 
-fn part_one() -> i64 {
-    let mut input = parse_input("input");
-    let monkeys = build_monkeys(&mut input);
-    for _ in 0..20 {
-        for i in 0..monkeys.len() {
-            monkeys[i].borrow_mut().do_monkey_business(&monkeys);
-        }
-    }
-    let solution = monkey_business(&monkeys);
-    println!("Solution part1: {}", solution);
-    return solution;
-}
-
-fn calculate_end_set_common_modul(monkeys: &Vec<RefCell<Monkey>>) {
-    let mut common_modul = 1;
-    for monkey in monkeys {
-        common_modul *= monkey.borrow().test;
-    }
-
-    for monkey in monkeys {
-        monkey.borrow_mut().devisor = 1;
-        monkey.borrow_mut().modulo_devisor = Some(common_modul);
-    }
+fn part_one() -> usize {
+    let mut map = parse_input("input");
+    println!("Max Steps: {}", map.max_steps);
+    let result = map.start_walking();
+    println!("Solution part1: {}", result);
+    return result;
 }
 
 fn part_two() -> i64{
-    let mut input = parse_input("input");
-    let monkeys = build_monkeys(&mut input);
-    calculate_end_set_common_modul(&monkeys);
-    for _ in 0..10000 {
-        for i in 0..monkeys.len() {
-            monkeys[i].borrow_mut().do_monkey_business(&monkeys);
-        }
-    }
-    let solution = monkey_business(&monkeys);
-    println!("Solution part2: {}", solution);
-    return solution;
+    return 0;
 }
 
-enum Part {
-    StartItem(i64),
-    Operation((Operation, Option<i64>)),
-    Test(i64),
-    TestTrue(usize),
-    TestFalse(usize),
-    Monkey
-}
-
-enum Operation {
-    Add,
-    Mult,
-}
-
-fn parse_starting_items(tokens: &mut SplitWhitespace) -> Vec<Part> {
+fn parse_input(filename: &str) -> Map {
     let mut result = Vec::new();
-    let second = tokens.nth(0).unwrap();
-    assert_eq!(second, "items:");
-
-    for number in tokens {
-        let number = String::from(number);
-        let number = number.trim_end_matches(',');
-        let number = number.parse::<i64>().unwrap();
-        result.push(Part::StartItem(number));
-    }
-    return result;
-}
-
-fn parse_operation(tokens: &mut SplitWhitespace) -> Part {
-    let second = tokens.nth(0).unwrap();
-    assert_eq!(second, "new");
-    tokens.nth(1);
-    let operator = tokens.nth(0).unwrap();
-    let operator = operator.chars().nth(0).unwrap();
-    let operator = match operator {
-        '*' => Operation::Mult,
-        '+' => Operation::Add,
-        _ => panic!("Can not be here")
-    };
-    let parameter = tokens.nth(0).unwrap();
-    let parameter = parameter.parse::<i64>();
-    if parameter.is_ok() {
-        return Part::Operation((operator, Some(parameter.unwrap())));
-    } else {
-        return Part::Operation((operator, None));
-    }
-}
-
-fn parse_test(tokens: &mut SplitWhitespace) -> Part {
-    let second = tokens.nth(0).unwrap();
-    assert_eq!(second, "divisible");
-    tokens.nth(0);
-    let divisor = tokens.nth(0).unwrap().parse::<i64>().unwrap();
-    return Part::Test(divisor);
-}
-
-fn parse_if(tokens: &mut SplitWhitespace) -> Part {
-    let which = tokens.nth(0).unwrap();
-    tokens.nth(2); //throw to monkey
-    let monkey =tokens.nth(0).unwrap().parse::<usize>().unwrap();
-    match which {
-        "true:" => Part::TestTrue(monkey),
-        "false:" => Part::TestFalse(monkey),
-        _ => panic!("Should not be here")
-    }
-}
-
-fn parse_input(filename: &str) -> Vec<Part> {
-    let mut result = Vec::new();
-    for line in read_lines(filename).unwrap() {
+    let mut start_index = (0, 0);
+    let mut target_index = (0, 0);
+    for (row_index, line) in read_lines(filename).unwrap().enumerate() {
         if let Ok(linedata) = line {
-            if linedata == "" {
-                continue;
+            let mut row = Vec::new();
+            for (column_index, element) in linedata.chars().enumerate() {
+                let elevation = match element {
+                    'S' => {start_index = (row_index, column_index); 0},
+                    'E' => {target_index = (row_index, column_index); 'z' as u32 - 'a' as u32},
+                    _ => {element as u32 - 'a' as u32},
+                };
+                row.push(elevation);
             }
-            let mut tokens = linedata.split_whitespace();
-            let first_token = tokens.nth(0).unwrap();
-            match first_token {
-                "Starting" => result.append(&mut parse_starting_items(&mut tokens)),
-                "Operation:" => result.push(parse_operation(&mut tokens)),
-                "Test:" => result.push(parse_test(&mut tokens)),
-                "If" => result.push(parse_if(&mut tokens)),
-                "Monkey" => result.push(Part::Monkey),
-                _ => panic!("Should not be here: {}", linedata)
-            };
+            result.push(row);
         }
     }
-    return result;
+    return Map::new(start_index, target_index, result);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -252,14 +230,49 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_possible_steps() {
+        let map = parse_input("test_input");
+        let psteps = map._possible_steps((0,0));
+        assert_eq!(psteps.len(), 2);
+
+        let ele = map.grid[1][2];
+        assert_eq!(ele, 2);
+
+        let psteps = map._possible_steps((1,2));
+        assert_eq!(psteps.len(), 3);
+
+        let psteps = map._possible_steps((2,5));
+        assert_eq!(psteps.len(), 4);
+    }
+
+    #[test]
+    fn test_input() {
+        let mut map = parse_input("test_input");
+        map.max_steps = 35;
+        let result = map.start_walking();
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn test_tuple_equal() {
+        let map = parse_input("test_input");
+        let result = map.start == (0,0);
+        assert_eq!(result, true);
+
+        let result = map.end == (2,5);
+        assert_eq!(result, true);
+    }
+
+
+    #[test]
     fn part_one_test() {
-        let solution = part_one();
-        assert_eq!(solution, 55216);
+        //let solution = part_one();
+        //assert_eq!(solution, 55216);
     }
 
     #[test]
     fn part_two_test() {
-        let solution = part_two();
-        assert_eq!(solution, 12848882750);
+        //let solution = part_two();
+        //assert_eq!(solution, 12848882750);
     }
 }
